b9cc18d54f7c43946b85aedb95ba23c3664674d1
Benjamin Schmidt <schmidtb@physics.mcgill.ca>
2013-05-14 19:57:52 -0400

    Modified simple.py with more examples of syntax

--- simple.py
+++ simple.py
@@ -8,10 +8,16 @@ Created on Mon May 13 20:11:33 2013
 from __future__ import division 
 import time
 
-for word in ['Hello', 'world!']:
+a = 4.56
+
+print "Hello world %.3f"%a
+
+    
+for idx, word in zip([1,3], ['Hello', 'world!']):
     
     #check if math is true
     if not 1/2 == 0:
         print word
-
+    print
+    
 print "finished at " + time.asctime(time.localtime())
